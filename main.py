96b3fdd1a201e930c01317d395fc663c5557ec60
MathiTC <mtcskole@gmail.com>
2023-02-22 11:30:34 +0100

    Talvalidation function indsat og adskillelse af funktionalitet og flow

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from datetime import datetime, timedelta #henter tiden
-def sleep_function():
-    while True: # Fortsætter indtil break modtages.
-        try:
-            sleep_hours = int(input("How many hours do you want to sleep? ")) # Spørg om antallet af timer der skal soves.
-            break # Stopper while true loopet
-        except ValueError: # Ved input fejl
-            print("Kun tal tilladt")
-    current_time = datetime.now()  # Nuværende tid kobles til variablen
-    wakeup_time = current_time + timedelta(hours=sleep_hours)  # Beregner hvornår du vågner igen.
-    print("Current time:", current_time.strftime("%H:%M:%S"))  # Printer nuværende tidspunkt
-    print("Wakeup time:", wakeup_time.strftime("%H:%M:%S"))  # Printer tiden efter du har sovet.
-    print("This is a function")
+current_time = datetime.now()  # Nuværende tid kobles til variablen
 
-sleep_function()
+# Kilde talvalidations kode https://github.com/Robotto/endnuNyereProjekt/blob/master/viTesterInput.py
+def talvalidation(x):
+    tastet = input(x)
+    try:
+        return float(tastet)
+    except ValueError:
+        print("Huskede du at skrive et tal? Prøv lige igen...")
+        return talvalidation(x)
+
+
+
+print("Welcome to the Sleep Calculator")
+sleep_hours = talvalidation("How many hours do you want to sleep?")
+wakeup_time = current_time + timedelta(hours=sleep_hours)  # Beregner hvornår du vågner igen.
+print("Current time:", current_time.strftime("%H:%M:%S"))  # Printer nuværende tidspunkt
+print("Wakeup time:", wakeup_time.strftime("%H:%M:%S"))  # Printer tiden efter du har sovet.
+print("Thanks for using the Sleep Calculator")
